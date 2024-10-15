5139db157997bffefe846c9410047e7af7e1300b
Aryaaazrr <94944128+Aryaaazrr@users.noreply.github.com>
2024-10-15 13:21:33 +0700

    set seed

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -2,10 +2,36 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Setting extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'key',
+        'locale',
+        'value',
+        'additional_value',
+        'json_value',
+        'additional_json_value',
+    ];
+
+    public function scopeKey($query, $key)
+    {
+        return $query->where('key', $key);
+    }
+
+    public function scopeLocale($query, $lang)
+    {
+        return $query->whereLocale($lang);
+    }
+
+    public function getJsonValueAttribute($value)
+    {
+        return json_decode($value);
+    }
+
+    public function getAdditionalJsonValueAttribute($value)
+    {
+        return json_decode($value);
+    }
 }

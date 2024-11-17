660b8a12a4f398845275290f07a4a683515e2c3f
Aryaaazrr <94944128+Aryaaazrr@users.noreply.github.com>
2024-11-17 15:23:19 +0700

    update step auth with laravel react

--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,27 @@
 <?php
 
-use App\Http\Controllers\Admin\Dashboard\DashboardController;
-use App\Http\Controllers\Website\Auth\AuthController;
-use App\Http\Controllers\Website\Home\HomeController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::middleware('guest')->group(function () {
-    Route::get('/', [HomeController::class, 'index'])->name('index');
-
-    Route::prefix('auth')->name('auth.')->group(function () {
-        Route::get('sign-in', [AuthController::class, 'index'])->name('index');
-        Route::get('sign-up', [AuthController::class, 'create'])->name('create');
-        Route::get('forgot', [AuthController::class, 'edit'])->name('edit');
-    });;
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
 
-Route::middleware('auth')->name('user.')->group(function () {
-    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__ . '/admin.php';
+require __DIR__.'/auth.php';
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,39 +1,22 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ["selector", '[data-mode="dark"]'],
     content: [
-        "./resources/**/*.blade.php",
-        "./resources/**/*.js",
-        "./resources/**/*.vue",
-        "./node_modules/flowbite/**/*.js",
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.jsx',
     ],
+    darkMode: 'class',
     theme: {
-        screens: {
-            sm: "480px",
-            md: "768px",
-            lg: "976px",
-            xl: "1440px",
-        },
         extend: {
-            spacing: {
-                128: "32rem",
-                144: "36rem",
-            },
-            borderRadius: {
-                "4xl": "2rem",
-            },
-            colors: {
-                primary: "#1570EF",
-                secondary: "#2B3674",
-                third: "#A3AED0",
-                hover: "#7E3F22",
-                dark: "#18181b",
-                navy: "#1B2559  ",
-                tosca: "rgba(95, 211, 208, 0.15)",
-                grey: "#F6F6F6",
-                wlp: "F4F7FE",
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
         },
     },
-    plugins: [require("@tailwindcss/forms"), require("flowbite/plugin")],
+
+    plugins: [forms],
 };
